42e7a28dc53246f75b3a1e306fc129feed18cc57
Fareed <far20220404@std.psut.edu.jo>
2025-04-19 18:37:43 +0300

    Add files via upload

---
+++ Validations.cpp
@@ -0,0 +1,49 @@
+#pragma once
+#include "Validations.h"
+using namespace std;
+namespace validations
+{
+	bool isLeapYear(int year)
+	{
+		if ((year % 400 == 0) || ((!(year % 100 == 0) && year % 4 == 0)))
+			return true;
+		else return false;
+	}
+
+	bool checkIfOdd(int c)
+	{
+		return !(c % 2 == 0);
+	}
+	bool checkIfEven(int c)
+	{
+		return !(checkIfOdd(c));
+	}
+	bool validateIntegerNumberInRange(int num, int from, int to)
+	{
+		return (num >= from && num <= to);
+	}
+	bool isPositive(double num)
+	{
+		return (num > 0);
+	}
+	bool isNonNegative(double n)
+	{
+		return (n >= 0);
+	}
+	bool isNegative(double n)
+	{
+		return (n < 0);
+	}
+	bool isUpperAlphabetical(char x)
+	{
+		return validateIntegerNumberInRange((int)x, 65, 90);
+	}
+	bool isLowerAlphabetical(char x)
+	{
+		return validateIntegerNumberInRange((int)x, 97, 122);
+	}
+	bool isAlphabetical(char x)
+	{
+		return (isLowerAlphabetical(x) || isUpperAlphabetical(x));
+	}
+}
